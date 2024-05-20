9a40ff87846f19663948c58c0f8636142732e0b4
alexisagood1 <70068418+alexisagood1@users.noreply.github.com>
2024-05-19 19:05:33 -0500

    Diamond.py

--- Diamond.py
+++ Diamond.py
@@ -1,1 +1,19 @@
+contact = "Email fieldalex08@gmail.com "8385393" "
+website = "Not supported yet"
+print("Version 1.0 beta")
+while True:
+    # take input from the user
+    choice = input("Enter choice: check in or tutorials? ")
 
+    # check if choice is one of the options
+    if choice in ('check in','tutorials'):
+        try:
+            Choice = float(input("Answer: ")
+        except ValueError:
+            print("Invalid input. Please enter a answer.")
+            continue
+
+        if choice == 'check in':
+            print(contact)
+        if choice == 'tutorial':
+            print(Website)
