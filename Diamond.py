91de92663f86081d01be8f3e4f03a0081fd0e5a3
alexisagood1 <70068418+alexisagood1@users.noreply.github.com>
2024-05-19 19:29:10 -0500

    Nothing to see here.py
    Dont open this

--- Diamond.py
+++ Diamond.py
@@ -1,19 +1,58 @@
-contact = "Email fieldalex08@gmail.com "8385393" "
-website = "Not supported yet"
-print("Version 1.0 beta")
-while True:
-    # take input from the user
-    choice = input("Enter choice: check in or tutorials? ")
+Print("We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
 
-    # check if choice is one of the options
-    if choice in ('check in','tutorials'):
-        try:
-            Choice = float(input("Answer: ")
-        except ValueError:
-            print("Invalid input. Please enter a answer.")
-            continue
+I just wanna tell you how I'm feeling
+Gotta make you understand
 
-        if choice == 'check in':
-            print(contact)
-        if choice == 'tutorial':
-            print(Website)
+Never gonna give you up, never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry, never gonna say goodbye
+Never gonna tell a lie and hurt you
+
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+
+And if you ask me how I'm feeling
+Don't tell me you're too blind to see
+
+Never gonna give you up, never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry, never gonna say goodbye
+Never gonna tell a lie and hurt you
+
+Never gonna give you up, never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry, never gonna say goodbye
+Never gonna tell a lie and hurt you
+
+(Ooh, give you up)
+(Ooh, give you up)
+(Ooh) never gonna give, never gonna give (give you up)
+(Ooh) never gonna give, never gonna give (give you up)
+
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+
+I just wanna tell you how I'm feeling
+Gotta make you understand
+
+Never gonna give you up, never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry, never gonna say goodbye
+Never gonna tell a lie and hurt you
+
+Never gonna give you up, never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry, never gonna say goodbye
+Never gonna tell a lie and hurt you
+
+Never gonna give you up, never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry, never gonna say goodbye
+Never gonna tell a lie and hurt you")
